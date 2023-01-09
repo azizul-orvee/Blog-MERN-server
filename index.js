ec3cc44753293971c9ee5f8c6e56fa63fab71b27
azizul-orvee <orvee16@gmail.com>
2023-01-10 02:20:23 +0600

    new DB added

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import postRoutes from './routes/posts.js';
-
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes); 
-
-
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
-  'mongodb+srv://azizulorvee:8mMxheDf@cluster0.bdrxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+  'mongodb+srv://azizulorvee:8mMxheDf@cluster0.jrujwgg.mongodb.net/?retryWrites=true&w=majority';
+
+  // 'mongodb+srv://azizulorvee:8mMxheDf@cluster0.bdrxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -25,6 +23,4 @@ mongoose
       console.log(`app listening on port ${PORT}`);
     })
   )
-    .catch((err) => console.log(err.message));
-  
-
+  .catch((err) => console.log(err.message));
